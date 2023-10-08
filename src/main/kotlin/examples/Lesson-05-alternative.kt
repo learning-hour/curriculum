70d490e643872777af0a784494f788c700db004b
Nasir Rasul <nasir@rasul.ca>
2023-10-08 14:33:35 -0600

    add else version

---
+++ src/main/kotlin/examples/Lesson-05-alternative.kt
@@ -0,0 +1,17 @@
+fun main() {
+    greetWithElse(40)
+    greetWithElse(20)
+    greetWithElse(14)
+    greetWithElse(13)
+    greetWithElse(12)
+    greetWithElse(10)
+}
+
+//note there is no else
+fun greetWithElse(age: Int) {
+    if (age > 13) {
+        println("secret kid greeting")
+    } else {
+        println("hello")
+    }
+}
